03669b719ed0fa455f98dfd8aa5ddcbd08c78ef2
Stefan Hajnoczi <stefanha@gmail.com>
2020-12-22 17:17:59 +0000

    qt: remove unused Qt features to minimize build
    Remove optional Qt features that are not needed by jammr. This makes the
    resulting Qt frameworks smaller and reduces the chances of compilation
    issues. Chromium in particular had build issues on macOS 10.13 and
    10.15.
    
    Signed-off-by: Stefan Hajnoczi <stefanha@gmail.com>

--- qt.sh
+++ qt.sh
@@ -21,6 +21,40 @@ cd qt-everywhere-*
 	    -no-rpath \
 	    -pkg-config \
 	    -no-dbus \
-	    -no-glib
-patch -p1 <../../qt-chromium-find_sdk.patch
+	    -no-glib \
+	    -skip qt3d \
+	    -skip qtactiveqt \
+	    -skip qtandroidextras \
+	    -skip qtcharts \
+	    -skip qtconnectivity \
+	    -skip qtdatavis3d \
+	    -skip qtdeclarative \
+	    -skip qtgamepad \
+	    -skip qtgraphicaleffects \
+	    -skip qtlocation \
+	    -skip qtlottie \
+	    -skip qtmultimedia \
+	    -skip qtnetworkauth \
+	    -skip qtpurchasing \
+	    -skip qtquick3d \
+	    -skip qtquickcontrols \
+	    -skip qtquickcontrols2 \
+	    -skip qtquicktimeline \
+	    -skip qtremoteobjects \
+	    -skip qtscript \
+	    -skip qtscxml \
+	    -skip qtsensors \
+	    -skip qtserialbus \
+	    -skip qtserialport \
+	    -skip qtspeech \
+	    -skip qtvirtualkeyboard \
+	    -skip qtwayland \
+	    -skip qtwebchannel \
+	    -skip qtwebengine \
+	    -skip qtwebglplugin \
+	    -skip qtwebsockets \
+	    -skip qtwebview \
+	    -skip qtwinextras \
+	    -skip qtx11extras \
+	    -skip qtxmlpatterns
 make all install
